971c36d32dd98dfead6cd35bd2b3443246742e3b
Raul Gomes Pimentel de Almeida <rgpa18@inf.ufpr.br>
2022-10-14 12:09:16 -0300

    first changes to use our own dataset classes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from torch import nn
 from model import FaceModel
 from options import opt
 import torchvision.utils as vutils
+import wandb
 import os
 import torch
 from statistics import PADMeter
@@ -11,6 +12,10 @@ import logging
 from tensorboardX import SummaryWriter
 from torch.utils.data.sampler import  WeightedRandomSampler
 from test import eval_model
+
+# our own datasets
+from liveness_datasets.datasets.casia_fasd import CASIAFASDDataset
+import torchvision.transforms as T
 
 file_name = os.path.join(opt.checkpoints_dir, opt.name,"log")
 logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
@@ -21,16 +26,58 @@ if not os.path.exists(run_dir):
     os.makedirs(run_dir)
 writer = SummaryWriter(log_dir=run_dir)
 
+wandb.init(project="DFA_fork")
 
 if __name__ == '__main__':
     best_res = 101
     train_batch_size = opt.batch_size
     test_batch_size = opt.batch_size
     
-    train_file_list = opt.train_file_list
-    dev_file_list = opt.dev_file_list
-    test_file_list = opt.test_file_list
     model = FaceModel(opt,isTrain = True,input_nc = 3)
+
+    # dataset setup
+    transform_src = T.Compose([
+        T.ToPILImage(),
+        T.Resize((256,256)),
+        T.ToTensor(),
+    ])
+
+    transform_depth = T.Compose([
+        T.ToPILImage(),
+        T.Resize((256,256)),
+        T.ToTensor(),
+        T.Lambda(lambda x: polarize(x)),
+    ])
+    train_dataset = CASIAFASDDataset(os.path.join("/home/raul/image_datasets/casia-new/data/"),
+                                     "train", transform=transform_src,
+                                     depth_transform=transform_depth,
+                                     # nodepth_path=os.path.join(
+                                        # DATA, "casia-new/data/attack_depth.png")
+                                     )
+    test_dataset = CASIAFASDDataset(os.path.join("/home/raul/image_datasets/casia-new/data/"),
+                                     "test", transform=transform_src,
+                                     depth_transform=transform_depth,
+                                     # nodepth_path=os.path.join(
+                                        # DATA, "casia-new/data/attack_depth.png")
+                                     )
+    dataset_sz = len(train_dataset)
+    indices = list(range(dataset_sz))
+    split = int(np.floor(.2 * dataset_sz))
+    if shuffle:
+        np.random.seed(143)
+        np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_indices)
+    val_sampler = SubsetRandomSampler(val_indices)
+    train_data_loader = DataLoader(train_dataset, batch_size=train_batch_size,
+                                   sampler=train_sampler, num_workers=8)
+    dev_data_loader = DataLoader(train_dataset, batch_size=test_batch_size,
+                                 sampler=val_sampler, num_workers=8) # using val as dev
+    test_data-loader = DataLoader(test_dataset, batch_size=test_batch_size, num_workers=8)
+
+    # TODO: report results on wandb
+
+    """
     test_data_loader = DataLoader(AlignedDataset(test_file_list,isTrain = False), batch_size=test_batch_size,
                                    shuffle=True, num_workers=8)
     dev_data_loader = DataLoader(AlignedDataset(dev_file_list,isTrain = False), batch_size=test_batch_size,
@@ -39,7 +86,9 @@ if __name__ == '__main__':
     train_dataset = AlignedDataset(train_file_list) 
     train_data_loader = DataLoader(train_dataset, batch_size=train_batch_size,
                                     shuffle = True,num_workers=8)
+                                    """
 
+    wandb.watch(model)
     writer.iter = 0
     for e in range(opt.epoch):
         model.train()
